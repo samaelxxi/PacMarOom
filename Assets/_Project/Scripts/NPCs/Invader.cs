60f43a302a654a73b478732a957ed1af23086def
Sami <samaelxxi@gmail.com>
2024-04-28 19:08:10 +0300

    invaders

--- Assets/_Project/Scripts/NPCs/Invader.cs
+++ Assets/_Project/Scripts/NPCs/Invader.cs
@@ -1,106 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Invader : NPC
+public class Invader : MonoBehaviour
 {
     [SerializeField] InvaderStats _stats;
     [SerializeField] InvaderProjectile _projectilePrefab;
     [SerializeField] Transform _shootPoint;
 
 
-    Fsm.State _idleState;
-    Fsm.State _patrolState;
-    Fsm.State _chaseState;
-    Fsm.State _attackState;
+    int _health;
+    float _shootTime;
 
-    protected override float RotationSpeed => _stats.RotationSpeed;
-    protected override float Speed => 0;
-    protected override float SightRange => _stats.SightRange;
-    protected override float ForgetRange => _stats.ForgetRange;
-
+    public void SetShootTime(float time) => _shootTime = time;
 
     // Start is called before the first frame update
     void Start()
     {
         _health = _stats.Health;
-
-        _idleState = Fsm_IdleState;
-        _chaseState = Fsm_ChaseState;
-
-        _fsm = new Fsm();
-        _fsm.Start(_idleState);
     }
 
-    // Update is called once per frame
-    protected override void Update()
+    public void TakeDamage(int damage)
     {
-        base.Update();
-        _fsm.OnUpdate();
+        _health -= damage;
+        if (_health <= 0)
+            Destroy(gameObject);
     }
 
-
-    float _idleStateTimer;
-    bool _idleMoveRight;
-    void Fsm_IdleState(Fsm fsm, Fsm.Step step, Fsm.State state)
+    public void Move(Vector3 displacement)
     {
-        if (step == Fsm.Step.Enter)
-        {
-            _idleStateTimer = 0;
-        }
-        if (step == Fsm.Step.Update)
-        {
-            _idleStateTimer += Time.deltaTime;
-            if (_idleStateTimer >= _stats.MoveCooldown)
-            {
-                transform.position = transform.position + _stats.MoveDist * (_idleMoveRight ? 1 : -1) * Vector3.right;
-                _idleMoveRight = !_idleMoveRight;
-                _idleStateTimer = 0;
-            }
-            if (_isAware)
-                fsm.TransitionTo(_chaseState);
-        }
+        transform.position += displacement;
     }
 
-
-    float _chaseStateTimer;
-    float _attackCooldownTimer;
-    void Fsm_ChaseState(Fsm fsm, Fsm.Step step, Fsm.State state)
+    public void PrepareShoot()
     {
-        if (step == Fsm.Step.Enter)
-        {
-            _chaseStateTimer = 0;
-            _attackCooldownTimer = 0;
-        }
-
-        if (step == Fsm.Step.Update)
-        {
-            _chaseStateTimer += Time.deltaTime;
-            _attackCooldownTimer += Time.deltaTime;
-            _targetPosition = Game.Instance.Pacman.transform.position;
-            RotateTowardsTarget();
-            if (_chaseStateTimer > _stats.MoveCooldown)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _stats.ForwardMoveDist);
-                _chaseStateTimer = 0;
-            }
-            if (_attackCooldownTimer > _stats.AttackCooldown)
-                Shoot();
-        }
+        this.InSeconds(UnityEngine.Random.Range(_shootTime - _shootTime*0.2f, _shootTime + _shootTime*0.2f), () => Shoot());
     }
 
     void Shoot()
     {
-        var projectile = Instantiate(_projectilePrefab, _shootPoint.position, transform.rotation);
+        var projectile = Instantiate(_projectilePrefab, _shootPoint.position, transform.rotation, transform);
         projectile.Setup(transform.forward, _stats.ProjectileSpeed, _stats.Damage);
-        _attackCooldownTimer = 0;
+        PrepareShoot();
     }
-
-    // void OnDrawGizmos()
-    // {
-    //     Gizmos.color = Color.red;
-    //     Gizmos.DrawWireSphere(transform.position, _stats.SightRange);
-    //     Gizmos.color = Color.blue;
-    //     Gizmos.DrawWireSphere(transform.position, _stats.ForgetRange);
-    // }
 }
